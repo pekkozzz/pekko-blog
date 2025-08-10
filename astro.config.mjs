1883f0ef1337672c49b5ed55211937b587222997
zengliq <zengliq@homeking365.com>
2025-08-10 17:47:18 +0800

    feat: 配置修改 bug修复和尾部样式

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://pekkozzz.github.io",
-	base: "/pekko-blog",
+	site: "http://blog.pekko.top",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
